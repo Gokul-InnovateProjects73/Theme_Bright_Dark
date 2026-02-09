c4dd0f68c88339f4d5b5ffa9fa8e159142dbd54e
Gokul-InnovateProjects73 <gokulmanivel73@gmail.com>
2026-02-09 15:52:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var body = document.querySelector("body");
+
+function dark() {
+  body.className = "";
+  body.classList.add("Dark");
+}
+
+function Light() {
+  body.className = "";
+  body.classList.add("Light");
+}
